0dad2df4c8b7933c23b14042fbdbeff76b5e0276
mvu <david.vu14@yahoo.de>
2017-02-23 14:33:43 +0100

    test1

---
+++ src/hallo.java
@@ -0,0 +1,11 @@
+
+public class hallo {
+
+	public static void halloDavid (){
+		System.out.println("Hallo von David");
+	}
+	
+	public static void halloWelt (){
+		System.out.println("Hallo von Welt");
+	}
+}
